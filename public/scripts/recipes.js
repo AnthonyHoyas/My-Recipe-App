2e3444023b868d1afdfcfa2312069e5184deb283
Anthony Hoyas <anthony.hoyas@gmail.com>
2022-03-11 19:36:30 +0100

    working

--- public/scripts/recipes.js
+++ public/scripts/recipes.js
@@ -11,7 +11,13 @@ let inputDescripton = document.getElementById('description')
 let inputIngredients= document.getElementById('ingredients')
 var TEXT1 = []
 let list = document.getElementById('anthony-kitchen-recipes')
-
+let burger = document.getElementById('svg')
+let carre = document.getElementById('carre')
+let recipecarre = document.getElementById('recipeCarre')
+let recipe = document.getElementById('recipe-book')
+let searchcarre = document.getElementById('searchCarre')
+let addcarre = document.getElementById('addCarre')
+let sendrecipecard = document.getElementById('send-recipe-card')
 
 
 
@@ -153,7 +159,78 @@ sendData.addEventListener('click', () => {
   addRecipeToDB(Array)
 })
 
+burger.addEventListener('click', () => {
+  if (carre.style.display == "none") {
+    carre.style.display = "flex"
+  } else {
+    carre.style.display = "none"
+  }
+})
+
+recipecarre.addEventListener('click', () => {
+  recipe.scrollIntoView({behavior: "smooth"})
+})
+
 let button = document.getElementById('button')
 button.addEventListener('click', () => {
   GetRecipeFromDB()
 })
+
+addcarre.addEventListener('click', () => {
+  sendrecipecard.scrollIntoView({behavior: "smooth"})
+})
+
+
+
+let FetchingData = () => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchcarre.value}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.meals[0]);
+    let recipeCard = `
+
+          <div id="recipe-book">
+          <div class="recipe-card" id="recipe-card-1">
+      
+            <aside>
+          
+              <img src="${data.meals[0].strMealThumb}" />
+          
+          
+            </aside>
+          
+            <article>
+          
+              <h2>${data.meals[0].strMeal}</h2>
+              <h3>${data.meals[0].strCategory}</h3>
+          
+              <ul>
+                <li><span class="icon icon-users"></span><span>1</span></li>
+                <li><span class="icon icon-clock"></span><span>X</span></li>
+                <li><span class="icon icon-level"></span><span>X</span></li>
+                <li><span class="icon icon-calories"></span><span>X</span></li>
+              </ul>
+          
+              <p>${data.meals[0].strInstructions}</p>
+          
+              <p class="ingredients"><span>Ingredients:&nbsp;</span>${data.meals[0].strIngredient1}</p>
+          
+            </article>
+          
+          </div>
+          
+        </div>`
+
+          list.insertAdjacentHTML('beforeend', recipeCard) 
+    
+  })
+
+}
+searchcarre.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    FetchingData(searchcarre.value)
+    recipe.scrollIntoView({behavior: "smooth"})
+
+
+  }
+});
